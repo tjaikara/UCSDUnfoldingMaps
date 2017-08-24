e5f72ce4d976ead30b420e25b3207fc14f7736dd
Tony Aikara <taikara@conversantmedia.com>
2017-08-23 20:31:56 -0500

    updated project from course

---
+++ src/module3/EarthquakeCityMap.java
@@ -0,0 +1,155 @@
+package module3;
+
+//Java utilities libraries
+import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Comparator;
+import java.util.List;
+
+//Processing library
+import processing.core.PApplet;
+
+//Unfolding libraries
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+
+//Parsing library
+import parsing.ParseFeed;
+
+/** EarthquakeCityMap
+ * An application with an interactive map displaying earthquake data.
+ * Author: UC San Diego Intermediate Software Development MOOC team
+ * @author Your name here
+ * Date: July 17, 2015
+ * */
+public class EarthquakeCityMap extends PApplet {
+
+	// You can ignore this.  It's to keep eclipse from generating a warning.
+	private static final long serialVersionUID = 1L;
+
+	// IF YOU ARE WORKING OFFLINE, change the value of this variable to true
+	private static final boolean offline = false;
+	
+	// Less than this threshold is a light earthquake
+	public static final float THRESHOLD_MODERATE = 5;
+	// Less than this threshold is a minor earthquake
+	public static final float THRESHOLD_LIGHT = 4;
+
+	/** This is where to find the local tiles, for working without an Internet connection */
+	public static String mbTilesString = "blankLight-1-3.mbtiles";
+	
+	// The map
+	private UnfoldingMap map;
+	
+	//feed with magnitude 2.5+ Earthquakes
+	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+
+	
+	public void setup() {
+		size(950, 600, OPENGL);
+
+		if (offline) {
+		    map = new UnfoldingMap(this, 200, 50, 700, 500, new MBTilesMapProvider(mbTilesString));
+		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
+		}
+		else {
+			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
+			//earthquakesURL = "2.5_week.atom";
+		}
+		
+	    map.zoomToLevel(2);
+	    MapUtils.createDefaultEventDispatcher(this, map);	
+			
+	    // The List you will populate with new SimplePointMarkers
+	    List<Marker> markers = new ArrayList<Marker>();
+
+	    //Use provided parser to collect properties for each earthquake
+	    //PointFeatures have a getLocation method
+	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+
+		for(PointFeature feature : earthquakes){
+			markers.add(createMarker(feature));
+		}
+	    
+	    // Add the markers to the map so that they are displayed
+	    map.addMarkers(markers);
+	}
+		
+	/* createMarker: A suggested helper method that takes in an earthquake 
+	 * feature and returns a SimplePointMarker for that earthquake
+	 * 
+	 * In step 3 You can use this method as-is.  Call it from a loop in the 
+	 * setp method.  
+	 * 
+	 * TODO (Step 4): Add code to this method so that it adds the proper 
+	 * styling to each marker based on the magnitude of the earthquake.  
+	*/
+	private SimplePointMarker createMarker(PointFeature feature)
+	{  
+		// To print all of the features in a PointFeature (so you can see what they are)
+		// uncomment the line below.  Note this will only print if you call createMarker 
+		// from setup
+		//System.out.println(feature.getProperties());
+		
+		// Create a new SimplePointMarker at the location given by the PointFeature
+		SimplePointMarker marker = new SimplePointMarker(feature.getLocation());
+		
+		Object magObj = feature.getProperty("magnitude");
+		float magnitude = Float.parseFloat(magObj.toString());
+		
+		// Here is an example of how to use Processing's color method to generate 
+	    // an int that represents the color yellow.  
+	    int yellow = color(255, 255, 0);
+
+		if(magnitude < THRESHOLD_LIGHT){
+			marker.setColor(color(0, 0, 255));
+			marker.setRadius(5.0f);
+		}
+		else if(magnitude >= THRESHOLD_LIGHT && magnitude < THRESHOLD_MODERATE){
+			marker.setColor(color(255, 255, 0));
+			marker.setRadius(8.0f);
+		}
+		else {
+			marker.setColor(color(255,0,0));
+			marker.setRadius(10.0f);
+		}
+
+	    return marker;
+	}
+	
+	public void draw() {
+	    background(10);
+	    map.draw();
+	    addKey();
+	}
+
+
+	// helper method to draw key in GUI
+	private void addKey() 
+	{
+		fill(255, 255, 204);
+		rect(20, 50, 160, 250);
+
+		fill(0,0,0);
+		text("Earthquake Key", 40, 100);
+		text("5.0+ Magnitude", 60, 140);
+		text("4.0+ Magnitude", 60, 180);
+		text("Below 4.0", 60, 220);
+
+		fill(255,0,0);
+		ellipse(45, 135, 10, 10);
+
+		fill(255,255,0);
+		ellipse(45, 176, 8, 8);
+
+		fill(0,0,255);
+		ellipse(45, 217.5f, 5, 5);
+
+	}
+}
